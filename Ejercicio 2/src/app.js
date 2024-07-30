aeabbc5c295054eb9fa41dda8caad1853553fa0c
Luca-Roman <rl.romanluca@gmail.com>
2024-07-30 17:35:11 -0300

    subo app de prueba

---
+++ Ejercicio 2/src/app.js
@@ -0,0 +1,11 @@
+import express from express;
+
+const PORT=3000
+
+const app=express()
+
+app.get("/", (req, res)=>{
+    res.send("Home Page Express Server");
+})
+
+app.listen(PORT, ()=>console.log(`Server online en puerto ${PORT}`))
